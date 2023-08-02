9b28f8df6c0b24bb636f062d66a3ee7270c066d4
Damla KS <damlaksrbst@gmail.com>
2023-08-02 23:36:33 +0300

    first commit

---
+++ CryptoCrazy/Model/Crypto.swift
@@ -0,0 +1,13 @@
+//
+//  Crypto.swift
+//  CryptoCrazy
+//
+//  Created by Damla KS on 2.08.2023.
+//
+
+import Foundation
+
+struct Crypto : Decodable {
+    let currency : String
+    let price : String
+}
